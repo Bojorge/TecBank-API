be558947439a21701fc95a0a784e0b27bf0817ee
gabriel conejo <gacova95@gmail.com>
2021-08-31 18:06:57 -0600

    prototipo de gestor de informacion
    se incluye:
    -archivo json de prueba
     el modelo de la entidad
    - la clase que accede al json y genera la lista en tiempo de ejecución

---
+++ TecBank API/Models/ModeloRol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecBank_API.Models
+{
+    public class ModeloRol
+    {
+        public ModeloRol(){}
+
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public override string ToString()
+        {
+            return "||" + this.Nombre + "   || " + this.Descripcion;
+        }
+    }
+}
---
+++ TecBank API/Rol.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TecBank_API.Models;
+
+namespace TecBank_API
+{
+    public class Rol
+    {
+        public List<ModeloRol> ListaDeRoles = new List<ModeloRol>();
+
+        public Rol()
+        {
+            listarRoles();
+        }
+        /**
+         * A partir de un archivo json se obtiene la coleccion de items (rol) y los serilaiza en una lista
+         * 
+         * **/
+        private void listarRoles()
+        {
+            using (StreamReader file = File.OpenText(@"..\..\..\roles.json"))//se puede usar la direccion exacta hay que ver donde poner el archivo
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                this.ListaDeRoles = (List<ModeloRol>)serializer.Deserialize(file, typeof(List<ModeloRol>));
+            }
+
+        }
+
+
+
+        /**
+         * muestra la lista en consola
+         * 
+         * **/
+        public void mostrarlista()
+        {
+            foreach (ModeloRol r in this.ListaDeRoles)
+            {
+                Console.WriteLine(r.ToString());
+            }
+        }
+        /**
+         * Agrega un item (rol) en la lista de colecciones y lo guarda en el archivo json
+         * 
+         * **/
+        public void agregarRol(string nombre, string descripcion)
+        {
+            ModeloRol rol = new ModeloRol();
+            rol.Nombre = nombre;
+            rol.Descripcion = descripcion;
+            this.ListaDeRoles.Add(rol);
+            guardarRol();
+        }
+        public void eliminarRol(string nombre)
+        {
+            int index = 0;
+            for (int i = 0; i < this.ListaDeRoles.Count; i++)
+            {
+                if (this.ListaDeRoles[i].Nombre == nombre)
+                {
+                    this.ListaDeRoles.RemoveAt(index);
+                    index = i;
+                    break;
+                }
+            }
+
+            guardarRol();
+        }
+        /**
+         * guarda la lista en archivo json comoc una coleccion
+         * 
+         * **/
+        private void guardarRol()
+        {
+            using (StreamWriter file = File.CreateText(@"..\..\..\roles.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, this.ListaDeRoles);
+            }
+        }
+
+        public void actualizarRol(string llave, string atributoAcambiar, string ValorParaCambiar)
+        {
+            int index = 0;
+            for (int i = 0; i < this.ListaDeRoles.Count; i++)
+            {
+                if (this.ListaDeRoles[i].Nombre == llave)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            ModeloRol r1 = this.ListaDeRoles[index];
+            switch (atributoAcambiar)
+            {
+                case "Nombre":
+                    r1.Nombre = ValorParaCambiar;
+                    break;
+                case "Descripcion":
+                    r1.Descripcion = ValorParaCambiar;
+                    break;
+                default:
+                    break;
+            }
+            guardarRol();
+        }
+    }
+}
